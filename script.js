44080696e54057823175841b72e45f9736b80452
joenhedav <235515282+joenhedav@users.noreply.github.com>
2026-01-21 11:47:06 -0300

    Add index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `"${this.title} by ${this.author}, ${this.pages}, ${this.read}"`;
+    }
+}
+
+const book1 = new Book("The Hobbit", "J.J.R. Tolkien", 295, "not read yet");
+console.log(book1.info());
