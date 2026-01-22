110536ae76f997e1e0a2767cbc081014601fe7ac
joenhedav <235515282+joenhedav@users.noreply.github.com>
2026-01-21 22:58:35 -0300

    Test

--- script.js
+++ script.js
@@ -9,10 +9,19 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return `"${this.title} by ${this.author}, ${this.pages}, ${this.read}"`;
-    }
 }
 
-const book1 = new Book("The Hobbit", "J.J.R. Tolkien", 295, "not read yet");
-console.log(book1.info());
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function renderLibrary() {
+    myLibrary.forEach(book => {
+        console.log(book);
+    });
+}
+
+addBookToLibrary("The Hobbit", "J.J.R. Tolkien", 295, "not read yet");
+
+renderLibrary();
